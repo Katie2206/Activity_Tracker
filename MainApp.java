56a3883650d832df5a5894621f79ad29ac05f940
Macsback <119421402+Macsback@users.noreply.github.com>
2023-10-19 12:38:32 +0100

    test
    test

--- MainApp.java
+++ MainApp.java
@@ -1,16 +1,5 @@
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+
     }
 }
