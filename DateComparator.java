273bdcaa75f852a5c5bc13a0482f5d202e183a20
Katie2206 <katielynch2204@gmail.com>
2023-11-01 22:52:41 +0000

    Created Date Comparator
    Created Date Comparator

--- DateComparator.java
+++ DateComparator.java
@@ -1,3 +1,7 @@
-public class DateComparator {
+import java.util.Comparator;
 
+public class DateComparator implements Comparator<Activity> {
+    public int compare(Activity date1, Activity date2){
+        return date1.getDate().compareTo(date2.getDate());
+    }
 }
