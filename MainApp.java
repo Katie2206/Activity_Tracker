28dbb444a3d725744f98953dd4187d26d13c96a4
Katie2206 <katielynch2204@gmail.com>
2023-11-01 15:47:58 +0000

    Added Method To Read Through Files
    Added Method That Checks For Data In Each Line And Stores The Data In A New ArrayList

--- MainApp.java
+++ MainApp.java
@@ -1,5 +1,24 @@
-public class MainApp {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class MainApp {
+    public static void readFromCSV (String file, ArrayList<Activity> activity, boolean headers) throws IOException {
+        File activityData = new File(file);
+        Scanner read = new Scanner(activityData);
+        String line;
+        boolean headersChecked = false;
+        while(read.hasNextLine()){
+            line = read.nextLine();
+            if(!headers || headers && headersChecked){
+                if(line!=""){
+                    Activity a = parseLine(line);
+                    activity.add(a);
+                }
+            }else{
+                headersChecked = true;
+            }
+        }
     }
 }
