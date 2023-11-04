b3045b31aad96c9a395aa0642f78d8096f26386d
Katie2206 <katielynch2204@gmail.com>
2023-11-04 22:41:14 +0000

    Added If Statement To Determine Running Intensity
    Added If Statement To Determine Running Intensity. Delete IntensityENUM Class And Moved The Enum Values To The Activity Class

--- running.java
+++ running.java
@@ -4,10 +4,28 @@ public class running extends Activity{
         super(ActivityType, Duration, Date, Distance, AverageHeartRate);
         this.kilometresPerHour = kmh;
     }
+
     public double getKilometresPerHour() {
         return kilometresPerHour;
     }
     public void setKilometresPerHour(double kilometresPerHour) {
         this.kilometresPerHour = kilometresPerHour;
     }
+
+    public Intensity getIntensity(){
+        if(kilometresPerHour < 4){
+            return Intensity.VERY_LIGHT;
+        }else if(kilometresPerHour >= 4 && kilometresPerHour < 8){
+            return Intensity.LIGHT;
+        }else if(kilometresPerHour >= 8 && kilometresPerHour < 12){
+            return Intensity.MODERATE;
+        }else if(kilometresPerHour >= 12 && kilometresPerHour < 16){
+            return Intensity.VIGOROUS;
+        }else if(kilometresPerHour >= 16 && kilometresPerHour < 24){
+            return Intensity.VERY_VIGOROUS;
+        }else{
+            return Intensity.VERY_VIGOROUS;
+        }
+    }
+
 }
