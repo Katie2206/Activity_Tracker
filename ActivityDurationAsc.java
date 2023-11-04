556f770eb567ee4c7b1dbd0f81ba3713dda8a709
Katie2206 <katielynch2204@gmail.com>
2023-11-04 14:46:26 +0000

    Added Class For Duration Descending And Altered Duration Ascending Code
    Added Class To Display The Activity Duration Descending. Added Menu Option To Display The Duration Descending.
    Altered Ascending Duration Code As It Was Displaying Incorrectly

--- ActivityDurationAsc.java
+++ ActivityDurationAsc.java
@@ -3,8 +3,7 @@ import java.util.Comparator;
 public class ActivityDurationAsc implements Comparator<Activity> {
     @Override
     public int compare(Activity a1, Activity a2){
-        int ascOrder = a2.getDuration() - a1.getDuration();
+        int ascOrder = a1.getDuration() - a2.getDuration();
         return ascOrder;
     }
-
 }
