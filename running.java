5eae853470ed547e42fd2f72bdc9e05a928dd2f7
Katie2206 <katielynch2204@gmail.com>
2023-11-04 20:36:08 +0000

    Created Class For Calculating Intensity Values For Running Data
    Created Class For Calculating Intensity Values, Added A New Filed For Kilometres Per Hour, Added Constructor For Running And Added A Getter And Setter For Kilometres Per Hour

--- running.java
+++ running.java
@@ -1,2 +1,13 @@
-public class running {
+public class running extends Activity{
+    private double kilometresPerHour;
+    public running(String ActivityType, int Duration, String Date, double Distance, int AverageHeartRate, double kmh){
+        super(ActivityType, Duration, Date, Distance, AverageHeartRate);
+        this.kilometresPerHour = kmh;
+    }
+    public double getKilometresPerHour() {
+        return kilometresPerHour;
+    }
+    public void setKilometresPerHour(double kilometresPerHour) {
+        this.kilometresPerHour = kilometresPerHour;
+    }
 }
